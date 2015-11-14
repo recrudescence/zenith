c8383b3d2e0684eafc771d1491db0b1177b66dd9
Victoria <vnqtran@brandeis.edu>
2015-11-14 13:08:36 -0500

    Update: mainPage Stuff

---
+++ app/src/main/java/io/codestellation/zenith/mainPage.java
@@ -0,0 +1,32 @@
+package io.codestellation.zenith;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.special.ResideMenu.ResideMenu;
+import com.special.ResideMenu.ResideMenuItem;
+
+public class mainPage extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        // Menu Drawer
+        ResideMenu resideMenu = new ResideMenu(this);
+        resideMenu.setBackground(R.drawable.menuBackground);
+        resideMenu.attachToActivity(this);
+
+        String titles[] = { "Home", "Profile", "Today's Goals", "Social", "History", "Settings" };
+        int icon[] = { R.drawable.iconHome, R.drawable.iconProfile, R.drawable.todaysGoals, R.drawable.social, R.drawable.history, R.drawable.settings };
+
+        int i;
+        for (i = 0; i < titles.length; i++){
+            ResideMenuItem item = new ResideMenuItem(this, icon[i], titles[i]);
+            item.setOnClickListener(this);
+            resideMenu.addMenuItem(item,  ResideMenu.DIRECTION_LEFT); // or  ResideMenu.DIRECTION_RIGHT
+        }
+    }
+
+}
