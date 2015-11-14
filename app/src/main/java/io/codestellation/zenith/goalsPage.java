e82df575ea85fe752f5456708bb8b96749ed53ef
Victoria <vnqtran@brandeis.edu>
2015-11-14 16:40:40 -0500

    goals Page and resizing of tempmainpagebackground

---
+++ app/src/main/java/io/codestellation/zenith/goalsPage.java
@@ -0,0 +1,15 @@
+package io.codestellation.zenith;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import io.codestellation.zenith.R;
+
+public class goalsPage extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goals_page);
+    }
+}
