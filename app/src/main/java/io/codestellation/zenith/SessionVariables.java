1d9afbbf7c1f6dd79470152f6c4f38df5f43f2da
myu28 <myu28@brandeis.edu>
2015-11-14 19:28:27 -0500

    added global variables

--- app/src/main/java/io/codestellation/zenith/SessionVariables.java
+++ app/src/main/java/io/codestellation/zenith/SessionVariables.java
@@ -1,44 +1,69 @@
 package io.codestellation.zenith;
 
+import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Michael on 11/14/2015.
  */
-public class SessionVariables {
-    static final String USER_EMAIL = "logged_in_email";
-    static final String LOGGEDIN_STATUS = "logged_in_status";
+public class SessionVariables extends Application {
+    public static SharedPreferences preferences;
+    static SharedPreferences.Editor editor = preferences.edit();
+    //global variables to be used across application
+    private static String[] todaysGoals;
+    private static ArrayList<String> goalHistory;
+    private static Date today;
+    private static ArrayList<String> menuOptions;
+    private static HashMap catExp;
+    private static final String logInEmail = "logInEmail";
+    private static final String logInState = "logInState";
 
-    public static SharedPreferences getSharedPreferences(Context ctx) {
-        return PreferenceManager.getDefaultSharedPreferences(ctx);
+
+    public static void initialize(){
+       /* preferences = getSharedPreferences(this.getApplicationContext());
+        editor.putString(logInEmail, null);
+        editor.putBoolean(logInState, false);
+        editor.commit();*/
+        today = new Date();
+        todaysGoals = new String[15];
+        goalHistory = new ArrayList<String>();
+        menuOptions = new ArrayList<String>(Arrays.asList("Today's Goals", "social", "History", "Settings"));
+        catExp  = new HashMap<String, Integer>();
     }
-    public static void setLoggedInUserEmail(TextView.OnEditorActionListener ctx, String email) {
-        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
-        editor.putString(USER_EMAIL, email);
+
+    public static SharedPreferences getSharedPreferences(Context applicationContext){
+        return applicationContext.getSharedPreferences("zenith",0);
+    }
+    public static void setLoggedInUserEmail(String email){
+        editor.putString(logInEmail, email);
+        editor.commit();
+    }
+    public static void setLoggedInState(Boolean loggedIn) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean(logInState, loggedIn);
         editor.commit();
     }
 
-    public static String getLoggedInEmailUser(Context ctx) {
-        return getSharedPreferences(ctx).getString(USER_EMAIL, "");
+    public static String[] getTodaysGoals(){
+        return todaysGoals;
+    }
+    public static ArrayList<String> getHistory(){
+        return goalHistory;
+    }
+    public static Date getDay(){
+        return today;
+    }
+    public static HashMap getCatExp(){
+        return catExp;
     }
 
-    public static void setUserLoggedInStatus(Context ctx, boolean status) {
-        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
-        editor.putBoolean(LOGGEDIN_STATUS, status);
-        editor.commit();
-    }
-
-    public static boolean getUserLoggedInStatus(Context ctx) {
-        return getSharedPreferences(ctx).getBoolean(LOGGEDIN_STATUS, false);
-    }
-
-    public static void clearLoggedInEmailAddress(Context ctx) {
-        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
-        editor.remove(USER_EMAIL);
-        editor.remove(LOGGEDIN_STATUS);
-        editor.commit();
-    }
 }
