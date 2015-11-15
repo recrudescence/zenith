055896bf637f8f0a129328d521e7379daa8f19ff
myu28 <myu28@brandeis.edu>
2015-11-15 00:44:14 -0500

    fixed issue with getting mission details

--- app/src/main/java/io/codestellation/zenith/SQLQueries.java
+++ app/src/main/java/io/codestellation/zenith/SQLQueries.java
@@ -2,24 +2,34 @@ package io.codestellation.zenith;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 
 /**
  * Created by Michael on 11/14/2015.
  */
 public class SQLQueries {
 
-    public String getMissionDetails(int i) throws SQLException {
-        String missionDetails = "";
+    //returns details of mission by {missionName, shortDesc, longDesc, exp, and category}
+    public HashMap<String, String> getMissionDetails(int i) throws SQLException {
+        HashMap<String, String> missionDetails = new HashMap<String, String>();
         DatabaseConnection connection = new DatabaseConnection();
+        //call query to get the information for mission with m_id = 1
         ResultSet rs = connection.sqlQuery(missionDetailsQuery(i));
         while(rs.next()){
+            //parse result and store all the fields
             String missionName = rs.getString(2).trim();
+            missionDetails.put("missionName", missionName);
             String shortDesc = rs.getString(3).trim();
+            missionDetails.put("shortDesc", shortDesc);
             String longDesc = rs.getString(4).trim();
-            int exp = rs.getInt(5);
+            missionDetails.put("longDesc", longDesc);
+            String exp = Integer.toString(rs.getInt(5));
+            missionDetails.put("exp", exp);
             ResultSet rsJoin = connection.sqlQuery(getCategoryName(i));
+            //run query to join m_c_id with categories to get category name
             while(rsJoin.next()) {
                 String category = rs.getString(1).trim();
+                missionDetails.put("category", category);
             }
 
         }
