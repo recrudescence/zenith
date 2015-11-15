c0d40a609b6ea1bc41d438d4a111aa0482b741b4
recrudescence <calvinmwang@gmail.com>
2015-11-15 01:13:57 -0500

    fixed module problems

--- app/src/main/java/io/codestellation/zenith/profilePage.java
+++ app/src/main/java/io/codestellation/zenith/profilePage.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.ImageView;
 
-public class profilePage extends ActionBarActivity {
+public class ProfilePage extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
